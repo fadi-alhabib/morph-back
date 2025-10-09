96de2db4fc67ed487ad03a139101f5f3b358e06d
fadi alhabib <fadi.b.alhabib@gmail.com>
2025-10-09 04:12:58 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
 
     /*
@@ -6,21 +7,28 @@ return [
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
     |
-    | Here you may configure your settings for cross-origin requests.
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
 
-    'paths' => ['api/*'],  // Apply CORS to all your API routes
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'], // GET, POST, PUT, DELETE, etc.
-    
-    'allowed_origins' => ['https://morphicarts.sa'], // Frontend URL only
-    // 'allowed_origins' => ['*'], // Only use '*' for testing; not for production
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
-    
-    'allowed_headers' => ['*'], // Allow all headers
+
+    'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true, // set true if you use cookies or auth headers
+
+    'supports_credentials' => true,
+
 ];
