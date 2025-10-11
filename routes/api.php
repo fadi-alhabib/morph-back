0d3cc3130951f39a7fd88ec7808bf6280a38eb96
fadi alhabib <fadi.b.alhabib@gmail.com>
2025-10-11 18:03:03 +0300

    login route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PerformerController;
 use Illuminate\Support\Facades\Route;
+
+Route::post("/login", [AuthController::class, "login"]);
 
 // Contact Us routes
 Route::post('/contacts', [ContactUsController::class, 'store']); // Create a contact
